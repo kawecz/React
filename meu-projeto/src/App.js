aa13ee866b8729d67b96e685dcb59348a5fb4080
Kawê Cezar <kawecezarjesus@gmail.com>
2025-05-12 15:36:35 -0500

    JSX aula

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 
 function App() {
+  const name = 'kawe';
+  
+
+  function sum(a,b) {
+    return a + b;
+  }
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <p>My first app.</p>
+      <p> Hello, my name is {name}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="oi" />  
     </div>
   );
 }
