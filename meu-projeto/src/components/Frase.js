bc6b5dd070fdabc31a181697f0455c9cc4404249
unknown <kawecezarjesus@gmail.com>
2025-09-11 17:56:52 -0500

    Updt

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -3,6 +3,7 @@ function Frase() {
     <div>
       <p>Este é um componente com uma frase!</p>
     </div>
+   
   );
 }
 export default Frase;
--- meu-projeto/src/components/frase2.js
+++
@@ -1,5 +0,0 @@
-function frase2() {
-    return( <p>and this is another phrase but in english.</p>);
-}
-
-export default frase2;
