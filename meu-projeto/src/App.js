009ef131d25850df3026112c3886074a7833b02a
unknown <kawecezarjesus@gmail.com>
2025-09-12 16:20:05 -0500

    Trabalhando com props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
-import Frase from "./components/Frase";
+import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
+
 
 function App() {
-  const name = "kawe";
-
+  const name = "maria"
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
-
-      <p> Hello, my name is {name}</p>
-      <HelloWorld />
-      <Frase/>
+      <SayMyName nome = "kawe" />
+      <SayMyName nome = "jão" />
+      <SayMyName nome = {name}/>
+      <Pessoa nome = "cabeção" 
+      profissao = "programador" 
+      idade = "20" 
+      foto = "https://www.shutterstock.com/image-photo/leipzig-germany-june-18-2024-600nw-2480563319.jpg"
+      />
     </div>
   );
 }
