2bc3d1cbefc6e4b4e026cb8c007de9b3f9c1017f
unknown <kawecezarjesus@gmail.com>
2025-09-16 15:09:16 -0500

    Avançando em props

--- meu_projeto/meu-projeto/src/App.jsx
+++ meu_projeto/meu-projeto/src/App.jsx
@@ -1,35 +1,26 @@
-import './App.css'
-import HelloWorld from './components/HelloWorld'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-import Button from './components/Button'
-import Lista from './components/List'
+import "./App.css";
+import HelloWorld from "./components/HelloWorld";
+import Pessoa from "./components/Pessoa";
+import Frase from "./components/Frase";
+import Button from "./components/Button";
+import Lista from "./components/List";
 
 function App() {
-  
-  const url = "https://plus.unsplash.com/premium_vector-1724311221938-e79529501a75?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
- 
+  const url =
+    "https://plus.unsplash.com/premium_vector-1724311221938-e79529501a75?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   return (
     <>
-      
-      <HelloWorld/>
-      <img src={url}/>   
+      <HelloWorld />
+      <img src={url} />
 
-      <Pessoa 
-        nome = "kawe"
-        idade = "20" 
-        sexo ="M" 
-        profissao = "Programador" 
-      />
-      <Frase nome = "kawe"/>
-      <Button/>
+      <Pessoa nome="kawe" idade="20" sexo="M" profissao="Programador" />
+      <Frase nome="kawe" />
+      <Button />
 
       <Lista/>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- meu_projeto/meu-projeto/src/components/List.jsx
+++ meu_projeto/meu-projeto/src/components/List.jsx
@@ -1,14 +1,16 @@
-import styles from "./Lista.module.css"
-import Item from "./Item"
+import styles from "./Lista.module.css";
+import Item from "./Item";
+
 function Lista() {
-    return (
-        <>
-            <ul className={styles.lista}>
-                <Item marca = "Ferrari" />
-                <Item marca = "Meta" />
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <ul className={styles.lista}>
+        <Item marca="Ferrari" ano_lancamento={1985} />
+        <Item />
+        <Item />
+      </ul>
+    </>
+  );
 }
 
-export default Lista
+export default Lista;
