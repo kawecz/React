cf47b120b6a1c315ee2f5bdc41194393f49a33f1
kawecz <kawecezarjesus@gmail.com>
2025-09-30 13:44:44 -0500

    aula 16 - react icons

--- meu_projeto/meu-projeto/src/components/layout/Footer.jsx
+++ meu_projeto/meu-projeto/src/components/layout/Footer.jsx
@@ -1,5 +1,14 @@
+import {FaFacebook,FaInstagram, FaLinkedin} from 'react-icons/fa'
+
 function Footer() {
-    return <footer>Rodapé</footer>
+    return (
+    <footer>
+        <ul>
+            <li><FaFacebook/> </li>
+            <li><FaInstagram/></li>
+            <li><FaLinkedin/></li>
+        </ul>
+    </footer>)
 }
 
 export default Footer
